f53ea62f99fb618e95d962cb0a1b7c4e6ea5065d
martaboso98 <frboruz@alumnos.ilernasevilla.com>
2023-11-05 20:08:46 +0100

    por fin funciona

--- script.js
+++ script.js
@@ -14,44 +14,76 @@ faceWithoutEyes.src = "yoSinOjos.png";
 //Iremos actualizando los valores
 const mousePosition = {x:0, y:0};
 
-//Radio de los ojos
-const eyeRadius = 
+const eyeRadius = 10;
 
 //Redimensionar el canvas
 function resizeCanvas () {
-    canvas.width = window.innerWidth; //Ancho interno de la pantalla
-    canvas.height = window.innerHeight; 
+    canvas.width = window.innerWidth * 0.7; //Ancho interno de la pantalla
+    canvas.height = window.innerHeight * 0.7; 
     context.height = canvas.height; //Resolución
     context.width = canvas.width;
+    render();
 }
 
 function drawFaceWithoutEyes () {
-    const x = canvas.width/2 - faceWithoutEyes.width/2; //Para ponerlo en medio
+    const x = canvas.width * 0.7 - faceWithoutEyes.width/2; //Para ponerlo en medio
     const y = canvas.height/2 - faceWithoutEyes.height/2;
     context.drawImage(faceWithoutEyes, x, y);
 }
 
+//Fórmula para calcular la distancia entre dos puntos
+function distance (a,b) {
+    return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
+}
+
+//Para ver dónde apunta el mouse, la dirección
+function getUnitVector (a,b) {
+    const module = distance(a,b);
+    return {
+        x: (b.x - a.x)/module,
+        y: (b.y - a.y)/module,
+    };
+}
+
+function getTranslatePosition(eyePosition){
+    //Si la posición del ratón está dentro del ojo, lo pinta ahí
+    const uniVector = getUnitVector (eyePosition, mousePosition);
+    return {
+        x: eyePosition.x + eyeRadius * uniVector.x,
+        y: eyePosition.y + eyeRadius * uniVector.y,
+    };
+}
+
 function drawEyes () {
-    const eyePositions = [ //Colocarlo donde yo quiera manualmente
+    const eyeOriginPositions = [ //Colocarlo donde yo quiera manualmente
         {
-            x:canvas.width/2 - eye.width/2 - 30,
-            y:canvas.height/2 - eye.width/2 - 26
+            x:canvas.width * 0.7 - 58,
+            y:canvas.height/2 
         },
         {
-            x:canvas.width/2 - eye.width/2 + 65,
-            y:canvas.height/2 - eye.width/2 - 26
+            x:canvas.width * 0.7 + 60,
+            y:canvas.height/2
         }
     ];
+
+    //Aplido la función a todos los elementos
+    const eyePositions = eyeOriginPositions.map(getTranslatePosition);
+
     //Recorrer array
     eyePositions.forEach((eyePosition) => {
-        context.drawImage(eye, eyePosition.x, eyePosition.y);
+        context.drawImage(
+            eye, 
+            eyePosition.x - eye.width/2, 
+            eyePosition.y - eye.height/2
+            );
     });
 }
 
-function drawMouse () {
+/*function drawMouse () {
     //Pinto un cuadrado para ver si funciona
     context.fillRect(mousePosition.x, mousePosition.y, 10, 10);
 }
+*/
 
 //Para borrar el rastro del ratón al moverse
 function clearCanvas () {
@@ -63,7 +95,7 @@ function render () {
     clearCanvas();
     drawFaceWithoutEyes();
     drawEyes();
-    drawMouse();
+    /*drawMouse();*/
 }
 
 function onResize () {
@@ -78,6 +110,13 @@ function onMouseMove (event) {
     render();
 }
 
+//Para móviles
+function onTouchMove (event) {
+    mousePosition.x = event.touches[0].clientX;
+    mousePosition.y = event.touches[0].clientY;
+    render();
+}
+
 //Función principal - main: se ejecuta cuando ya ha cargado la página
 function main () {
     resizeCanvas();
@@ -85,6 +124,7 @@ function main () {
     //Evento para que se redimensione cada vez que cargue para que sea reponsive
     window.addEventListener("resize", onResize);
     window.addEventListener("mousemove", onMouseMove);
+    window.addEventListener("touchmove", onTouchMove);
 }
 
 window.onload = main;
