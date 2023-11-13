0829d1537de56c6f365e0a7d2dca74ea2709202f
Marta Borreguero Soria <martabo_so@hotmail.com>
2023-11-13 14:20:55 +0100

    canvas

--- script.js
+++ script.js
@@ -4,50 +4,50 @@ const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
 //Crear objeto imagen
-const eye = new Image ();
+const eye = new Image();
 eye.src = "ojo.png";
 
 //Ojos
-const faceWithoutEyes = new Image ();
+const faceWithoutEyes = new Image();
 faceWithoutEyes.src = "yoSinOjos.png";
 
 //Iremos actualizando los valores
-const mousePosition = {x:0, y:0};
+const mousePosition = { x: 0, y: 0 };
 
-const eyeRadius = 10;
+let eyeRadius = 10;
 
 //Redimensionar el canvas
 function resizeCanvas () {
-    canvas.width = window.innerWidth * 0.7; //Ancho interno de la pantalla
-    canvas.height = window.innerHeight * 0.7; 
+    canvas.width = window.innerWidth; //Ancho interno de la pantalla
+    canvas.height = window.innerHeight; 
     context.height = canvas.height/2; //Resolución
     context.width = canvas.width/2;
     render();
 }
 
 function drawFaceWithoutEyes () {
-    const x = canvas.width * 0.63 - faceWithoutEyes.width/2; //Para ponerlo en medio
+    const x = canvas.width/2 - faceWithoutEyes.width/2; //Para ponerlo en medio
     const y = canvas.height/2 - faceWithoutEyes.height/2;
-    context.drawImage(faceWithoutEyes, x, y);
+    context.drawImage(faceWithoutEyes, x, y, 700, 700);
 }
 
 //Fórmula para calcular la distancia entre dos puntos
-function distance (a,b) {
+function distance(a, b) {
     return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
 }
 
 //Para ver dónde apunta el mouse, la dirección
-function getUnitVector (a,b) {
-    const module = distance(a,b);
+function getUnitVector(a, b) {
+    const module = distance(a, b);
     return {
-        x: (b.x - a.x)/module,
-        y: (b.y - a.y)/module,
+        x: (b.x - a.x) / module,
+        y: (b.y - a.y) / module,
     };
 }
 
-function getTranslatePosition(eyePosition){
+function getTranslatePosition(eyePosition) {
     //Si la posición del ratón está dentro del ojo, lo pinta ahí
-    const uniVector = getUnitVector (eyePosition, mousePosition);
+    const uniVector = getUnitVector(eyePosition, mousePosition);
     return {
         x: eyePosition.x + eyeRadius * uniVector.x,
         y: eyePosition.y + eyeRadius * uniVector.y,
@@ -57,11 +57,11 @@ function getTranslatePosition(eyePosition){
 function drawEyes () {
     const eyeOriginPositions = [ //Colocarlo donde yo quiera manualmente
         {
-            x:canvas.width * 0.63 - 58,
+            x:canvas.width/2 - 54,
             y:canvas.height/2 
         },
         {
-            x:canvas.width * 0.63 + 60,
+            x:canvas.width/2 + 60,
             y:canvas.height/2
         }
     ];
@@ -86,8 +86,8 @@ function drawEyes () {
 */
 
 //Para borrar el rastro del ratón al moverse
-function clearCanvas () {
-    context.clearRect(0,0, canvas.width, canvas.height);
+function clearCanvas() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 //Cuando hago resize, desaparece el dibujo. Para corregirlo:
@@ -109,6 +109,7 @@ function onMouseMove (event) {
     mousePosition.y = event.offsetY;
     render();
 }
+
 
 //Para móviles
 function onTouchMove (event) {
